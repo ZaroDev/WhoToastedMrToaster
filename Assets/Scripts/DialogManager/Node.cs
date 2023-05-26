f3388113d146623c62abc70835f6e878ef665323
Xymaru <61521967+Xymaru@users.noreply.github.com>
2023-05-26 12:02:20 +0200

    Update Node.cs

--- Assets/Scripts/DialogManager/Node.cs
+++ Assets/Scripts/DialogManager/Node.cs
@@ -5,14 +5,59 @@ using System.Diagnostics;
 using UnityEditor;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+
 namespace WhoToastedMrToaster.DialogManager
 {
+    public class NodeOption
+    {
+        public string text;
+        public Node node;
+        public Action callback;
+    }
+
     public class Node
     {
-        string Name;
-        string Dialogue;
+        public string Title;
+        public string Dialogue;
 
-        List<Node> Nodes = new List<Node>();
-        List<Action> Callbacks = new List<Action>();
+        public List<NodeOption> Options = new List<NodeOption>();
+    }
+
+    public class DialogManager
+    {
+        List<Node> m_Nodes = new List<Node>();
+        Node m_ActiveNode = null;
+
+        public void SetStartingNode(int index)
+        {
+            if (index < 0 || index >= m_Nodes.Count) return;
+
+            m_ActiveNode = m_Nodes[index];
+        }
+
+        public Node CreateNode()
+        {
+            Node node = new Node();
+
+            m_Nodes.Add(node);
+
+            return node;
+        }
+
+        public Node SelectedOption(int option)
+        {
+            if (m_ActiveNode == null) return null;
+            if (option < 0 || option >= m_ActiveNode.Options.Count) return null;
+
+            m_ActiveNode.Options[option].callback();
+            m_ActiveNode = m_ActiveNode.Options[option].node;
+
+            return m_ActiveNode;
+        }
+
+        public Node getActiveNode()
+        {
+            return m_ActiveNode;
+        }
     }
 }
