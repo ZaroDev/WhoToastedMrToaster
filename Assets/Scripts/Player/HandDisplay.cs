2aadff4691d8a9341fb6fa7653c8317e4289b6c1
Victor <victorfalcon796@gmail.com>
2023-06-05 18:44:10 +0200

    mmmh que rico que esta la cosa

--- Assets/Scripts/Player/HandDisplay.cs
+++ Assets/Scripts/Player/HandDisplay.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,8 +7,12 @@ namespace WhoToastedMrToaster
 {
     public class HandDisplay : MonoBehaviour
     {
+        public float waitTime = 3f;
         public List<Toggle> objectives = new List<Toggle>();
+        public AudioSource source;
+        public GameObject glowingObject;
 
+        private Coroutine unlockCoroutine = null;
         private void OnEnable()
         {
             GameManager.OnUnlockObjective += UnlockNextObjective;
@@ -19,15 +24,48 @@ namespace WhoToastedMrToaster
 
         public void Awake()
         {
+            source = GetComponent<AudioSource>();
             foreach (var objective in objectives)
             {
                 objective.isOn = false;
+            }
+        }
+        int obj = 0;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                UnlockNextObjective(obj++);
             }
         }
 
         public void UnlockNextObjective(int objective)
         {
             objectives[objective].isOn = true;
+
+            source.Play();
+            if (unlockCoroutine != null)
+            {
+                StopCoroutine(unlockCoroutine);
+            }
+            unlockCoroutine = StartCoroutine(UnlockCoroutine(waitTime));
+        }
+
+        private IEnumerator UnlockCoroutine(float waitTime)
+        {
+            float endTime = Time.time + waitTime;
+            WaitForSeconds timeToWait = new WaitForSeconds(0.2f);
+
+            bool active = true;
+            while (Time.time < endTime)
+            {
+                glowingObject.SetActive(active);
+                active = !active;
+
+                yield return timeToWait;
+            }
+            glowingObject.SetActive(false);
+            unlockCoroutine = null;
         }
     }
 }
